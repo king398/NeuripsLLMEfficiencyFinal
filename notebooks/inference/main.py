9d01d23be6596f611480449045b7fbbd6c843770
mithil <mithilsalunkhe@outlook.com>
2023-09-15 18:25:42 +0530

    added more eval models

--- notebooks/inference/main.py
+++ notebooks/inference/main.py
@@ -1,2 +1,13 @@
-if __name__ == '__main__':
-    print("Hello World")
+from fastapi import FastAPI
+
+import logging
+import os
+import time
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+logger = logging.getLogger(__name__)
+# Configure the logging module
+logging.basicConfig(level=logging.INFO)
+model_name = "Mithilss/Llama-2-7b-hf"
